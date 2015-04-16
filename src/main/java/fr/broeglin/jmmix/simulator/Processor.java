964d3cc1f6b957d23c960ddaa3d69adc9b61117f
Dominique Broeglin <dominique.broeglin@gmail.com>
2015-04-16 22:05:55 +0200

    Added default values for constant registers

--- src/main/java/fr/broeglin/jmmix/simulator/Processor.java
+++ src/main/java/fr/broeglin/jmmix/simulator/Processor.java
@@ -5,38 +5,101 @@ public class Processor {
 	public static final int NB_REGISTERS = 256;
 	private final long[] registers = new long[NB_REGISTERS];
 	private boolean running = true;
-	
-	long rB;
-	long rD;
-	long rE;
-	long rH;
-	long rJ;
-	long rM;
-	long rR;
-	long rBB;
-	long rC;
-	long rN;
-	long rO;
-	long rS;
-	long rI;
-	long rT;
-	long rTT;
-	long rK;
-	long rQ;
-	long rU;
-	long rV;
-	long rG;
-	long rL;
+
+	// Arithmetic status register
 	long rA;
-	long rF;
+	
+	// Bootstrap register
+	long rB;
+	
+	// Continuation register
+	long rC;
+	
+	// Dividend register
+	long rD;
+	
+	// Epsilon register
+	long rE;
+	
+	// Failure location register
+	long rF = 0l; // constant
+	
+	// Global threshold register
+	long rG;
+	
+	// Himult register
+	long rH;
+
+	// Interval counter
+	long rI;
+	
+	// Return-jump register
+	long rJ;
+	
+	// Interrupt mask register
+	long rK = 0xffffffffffffffffl; // constant
+	
+	// Local threshold register
+	long rL;
+	
+	// Multiplex mask register
+	long rM;
+	
+	// Serial number
+	long rN;
+	
+	// Register stack offset
+	long rO;
+	
+	// Prediction register
 	long rP;
+
+	// Interrupt request register
+	long rQ = 0l; // constant
+	
+	// Remainder register
+	long rR;
+	
+	// Register stack pointer
+	long rS;
+	
+	// Trap address register
+	long rT = 0x8000000500000000l; // constant
+	
+	// Usage counter
+	long rU;
+
+	// Virtual translation register
+	long rV = 0x369c200400000000l; // constant
+
+	// Where-interrupted register
 	long rW;
+	
+	// Execution register
 	long rX;
+	
+	// Y operand
 	long rY;
+	
+	// Z operand
 	long rZ;
+
+	// Bootstrap register
+	long rBB;
+
+	// Dynamic trap address register
+	long rTT = 0x8000000600000000l; // constant
+	
+	// Where-interrupted register
 	long rWW;
+	
+	// Execution register
 	long rXX;
+	
+	// Y operand
 	long rYY;
+	
+	// Z operand
 	long rZZ;
 
 	public long register(int i) {
